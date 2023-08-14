29faded69d48470c3af781b72d8335bf37dd9714
Brandon Nestor Reis <brandon.nestor.reis@gmail.com>
2023-08-14 10:40:56 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function saturdayFun(activity) {
+    if (typeof activity === 'undefined') {
+        return 'This Saturday, I want to roller-skate!'
+    }
+    else
+    return `This Saturday, I want to ${activity}!`
+}
+
+const monday = mondayWork(activity)
+function mondayWork(activity) {
+    if (typeof activity === 'undefined') {
+        return "This Monday, I will go to the office."
+    }
+    else
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(wrap = '*') {
+    return function (adjective = 'special') {
+      return `You are ${wrap}${adjective}${wrap}!`;
+      };
+    }
